d6b12774ac4067ca6cd79fbf5b524423affd1d76
Eric Gregório De Aquino <gregorio.eric@gmail.com>
2024-02-06 17:44:39 +0000

    Criado Pagina home e criado Header Footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import "./style.css";
 
 function Header() {
   return (
-    <header>
-      <h1>header</h1>
+    <header className="header">
+      <div className="content content-header">
+        <h1>header</h1>
+      </div>
     </header>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+function Home() {
+  return (
+    <main className="home">
+      <div className="content content-home">
+        <h1>Home</h1>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
