f2e9db8bab37a52e84ddb559a4700149442e3c6a
Eric Gregório De Aquino <gregorio.eric@gmail.com>
2024-02-08 22:31:30 +0000

    Criado componente Loading e atualizado page Home

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./loading.css";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <span>Carregando...</span>
+    </div>
+  );
+}
+
+export default Loading;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,49 @@
+import { useContext, useState } from "react";
 import "./style.css";
+import context from "../../context/Context";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../components/Loading/Loading";
+import Cards from "../../components/Cards/Cards";
 
 function Home() {
+  const { dataHome, setSearch } = useContext(context);
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSearch = async () => {
+    const req = await fetch(
+      `https://www.omdbapi.com/?apikey=58eeaad1&s=${input}`
+    );
+    const res = await req.json();
+    const data = res.Search;
+
+    setSearch(data);
+    setInput("");
+
+    navigate(`/search/${data[0].Title}`);
+  };
+
+  if (!dataHome) {
+    return <Loading />;
+  }
+
   return (
     <main className="home">
       <div className="content content-home">
         <h1>Home</h1>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Encontre Filmes, Series e Animações..."
+            value={input}
+            onChange={({ target }) => setInput(target.value)}
+          />
+          <button onChange={handleSearch}>Search</button>
+        </div>
+        <div className="content-cards">
+          <Cards />
+        </div>
       </div>
     </main>
   );
