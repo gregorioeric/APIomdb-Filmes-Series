0640ed075c9ffbe3aa3ad75aa621d9a76e7dd300
Eric Gregório De Aquino <gregorio.eric@gmail.com>
2024-02-08 15:36:15 +0000

    Criado Componente Cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,39 @@
+import "./cards.css";
+
+function Cards({ data }) {
+  return (
+    <>
+      <div>
+        {data.map((e, i) => (
+          <div key={i}>
+            <h2>{e.title}</h2>
+            <div className="card-row">
+              {e.movies.map((movie, index) => (
+                <div key={index} className="cards">
+                  <picture>
+                    <img src={movie.Poster} alt="" />
+                  </picture>
+                  <h2>{movie.Title}</h2>
+                  <p>
+                    <strong>Released: </strong>
+                    {movie.Released}
+                  </p>
+                  {movie.Plot && (
+                    <div className="plot-content">
+                      <p>
+                        <strong>Description:</strong>
+                      </p>
+                      <p className="plot">{movie.Plot}</p>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Cards;
